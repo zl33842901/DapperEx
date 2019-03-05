b9d3a45728632c9d810d0dd80727eba0c6ec9ee7
zl33842901 <zhanglei33842901@qq.com>
2019-03-06 00:00:26 +0800

    t

--- xLiAd.DapperEx.PostgreSql.Samples/Program.cs
+++ xLiAd.DapperEx.PostgreSql.Samples/Program.cs
@@ -11,6 +11,12 @@ namespace xLiAd.DapperEx.PostgreSql.Samples
         optionA = 1,
         optionB = 2
     }
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
     [Table("News", Schema="public")]
     public class TTTTTTtest
     {
@@ -19,6 +25,7 @@ namespace xLiAd.DapperEx.PostgreSql.Samples
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public Author Author { get; set; }
     }
     [Table("DictInfo", Schema = "public")]
     public class DictInfo
@@ -47,12 +54,13 @@ namespace xLiAd.DapperEx.PostgreSql.Samples
             var constring = "Host=127.0.0.1;Port=5432;Database=zhanglei;Username=postgres;Password=zhanglei";
             RepositoryPg<TTTTTTtest> repository = new RepositoryPg<TTTTTTtest>(constring);
             RepositoryPg<DictInfo> repod = new RepositoryPg<DictInfo>(constring);
-            //TTTTTTtest ttest = new TTTTTTtest()
-            //{
-            //    Content = "小喇叭开始广播啦",
-            //    Title = "哒滴滴哒滴滴"
-            //};
-            //var iiid = repository.Add(ttest);
+            TTTTTTtest ttest = new TTTTTTtest()
+            {
+                Content = "pg大法",
+                Title = "哒滴滴哒滴滴",
+                Author = new Author() { BirthDay = DateTime.Now, Id = 5, Name="张磊"}
+            };
+            var iiid = repository.Add(ttest);
 
             //var l = repository.Where(x => true);
             //var r225 = repository.WhereSelect(x => x.Id >= 3, x => x.Title);
@@ -65,22 +73,22 @@ namespace xLiAd.DapperEx.PostgreSql.Samples
             //var r443 = repository.Update(ddxx, x=>x.Content);
 
             //var l = repository.PageList(x => x.Id > 1, x=>x.Id , 2, 3, true);
-            var trans = repository.GetTransaction();
-            var trepo = trans.GetRepository<DictInfo>();
-            var trepo2 = trans.GetRepository<TTTTTTtest>();
-            try
-            {
-                trepo.UpdateWhere(x => x.DictID == 2, x => x.CreateTime, DateTime.Now);
-                var i = 0;
-                var tijwoeifj = trepo.Where(x => x.DictID == 3); //如果要使用
-                //var j = 5 / i;
-                trepo2.UpdateWhere(x => x.Id == 3, x => x.Title, "个人资料类别");
-                trans.Commit();
-            }
-            catch (Exception eeeee)
-            {
-                trans.Rollback();
-            }
+            //var trans = repository.GetTransaction();
+            //var trepo = trans.GetRepository<DictInfo>();
+            //var trepo2 = trans.GetRepository<TTTTTTtest>();
+            //try
+            //{
+            //    trepo.UpdateWhere(x => x.DictID == 2, x => x.CreateTime, DateTime.Now);
+            //    var i = 0;
+            //    var tijwoeifj = trepo.Where(x => x.DictID == 3); //如果要使用
+            //    //var j = 5 / i;
+            //    trepo2.UpdateWhere(x => x.Id == 3, x => x.Title, "个人资料类别");
+            //    trans.Commit();
+            //}
+            //catch (Exception eeeee)
+            //{
+            //    trans.Rollback();
+            //}
         }
     }
 }
