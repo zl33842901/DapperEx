675bf790d5cd6dbbef83c6679c2a5433df9050a3
zl33842901 <zhanglei33842901@qq.com>
2019-07-02 23:50:57 +0800

    减少二义性

--- xLiAd.DapperEx.Repository/Repository.cs
+++ xLiAd.DapperEx.Repository/Repository.cs
@@ -53,7 +53,7 @@ namespace xLiAd.DapperEx.Repository
         /// <param name="exceptionHandler"></param>
         /// <param name="throws"></param>
         /// <param name="_tran">事务</param>
-        public Repository(IDbConnection _con, RepoXmlProvider repoXmlProvider = null, MsSql.Core.Core.DapperExExceptionHandler exceptionHandler = null, bool throws = true, IDbTransaction _tran = null)
+        public Repository(IDbConnection _con, RepoXmlProvider repoXmlProvider, MsSql.Core.Core.DapperExExceptionHandler exceptionHandler, bool throws, IDbTransaction _tran)
             : base(_con, repoXmlProvider, exceptionHandler, throws, _tran)
         {
 
